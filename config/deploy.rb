21042741b442d8bf89d62011c11dfdc83cc5adfa
Steven Ko <chunyuko85@gmaill.com>
2013-01-16 17:45:06 +0800

    mysql

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,25 +1,40 @@
-set :application, "set your application name here"
-set :repository,  "set your repository location here"
+require 'bundler/capistrano'
+# require 'hoptoad_notifier/capistrano'
 
-# set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
-# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+set :application, "testapp"
+set :rails_env, "production"
 
-role :web, "your web-server here"                          # Your HTTP server, Apache/etc
-role :app, "your app-server here"                          # This may be the same as your `Web` server
-role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
-role :db,  "your slave db-server here"
+set :branch, "master"
+set :repository,  "https://github.com/StevenKo/testapp.git"
+set :scm, "git"
+set :user, "apps" # 一個伺服器上的帳戶用來放你的應用程式，不需要有sudo權限，但是需要有權限可以讀取Git repository拿到原始碼
 
-# if you want to clean up old releases on each deploy uncomment this:
-# after "deploy:restart", "deploy:cleanup"
+set :deploy_to, "/home/apps/testapp"
+set :deploy_via, :remote_cache
+set :use_sudo, false
 
-# if you're still using the script/reaper helper you will need
-# these http://github.com/rails/irs_process_scripts
+role :web, "106.187.103.131"
+role :app, "106.187.103.131"
+role :db,  "106.187.103.131", :primary => true
 
-# If you are using Passenger mod_rails uncomment this:
-# namespace :deploy do
-#   task :start do ; end
-#   task :stop do ; end
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#   end
-# end
+namespace :deploy do
+
+  task :copy_config_files, :roles => [:app] do
+    db_config = "#{shared_path}/database.yml"
+    run "cp #{db_config} #{release_path}/config/database.yml"
+  end
+  
+  task :update_symlink do
+    run "ln -s {shared_path}/public/system {current_path}/public/system"
+  end
+  
+  task :start do ; end
+  task :stop do ; end
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+  end
+end
+
+before "deploy:assets:precompile", "deploy:copy_config_files" # 如果將database.yml放在shared下，請打開
+after "deploy:update_code", "deploy:copy_config_files" # 如果將database.yml放在shared下，請打開
+# after "deploy:finalize_update", "deploy:update_symlink" # 如果有實作使用者上傳檔案到public/system，請打開
